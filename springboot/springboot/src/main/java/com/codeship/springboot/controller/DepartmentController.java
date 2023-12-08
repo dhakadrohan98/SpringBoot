341c8b3627ec026f684994abf93cc61970d07768
unknown <dhakadrohan98>
2023-12-08 20:34:04 +0530

    REST Post and GET APIs are created in springboot & executed through postman

---
+++ springboot/springboot/src/main/java/com/codeship/springboot/controller/DepartmentController.java
@@ -0,0 +1,29 @@
+package com.codeship.springboot.controller;
+
+import com.codeship.springboot.entity.Department;
+import com.codeship.springboot.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class DepartmentController {
+
+    @Autowired
+    private DepartmentService departmentService;
+    @PostMapping("/create/department")
+    public Department saveDepartment(@RequestBody Department department) {
+        return departmentService.saveDepartment(department);
+    }
+
+    @GetMapping("/departments")
+    public List<Department> fetchDepartmentList() {
+        return departmentService.fetchDepartmentList();
+    }
+
+    @GetMapping("/departments/{id}")
+    public Department fetchDepartmentById(@PathVariable("id") Long departmentId) {
+        return departmentService.fetchDepartmentById(departmentId);
+    }
+}
