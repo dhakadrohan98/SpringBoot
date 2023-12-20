673df8e7e74ba064c50a5de33b849e5e959f57c0
unknown <dhakadrohan98>
2023-12-20 18:09:01 +0530

    REST APIs to create books & get books

--- graphql-project/src/main/java/com/graphql/learn/GraphqlProjectApplication.java
+++ graphql-project/src/main/java/com/graphql/learn/GraphqlProjectApplication.java
@@ -1,14 +1,31 @@
 package com.graphql.learn;
 
+import com.graphql.learn.entities.Book;
+import com.graphql.learn.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GraphqlProjectApplication {
+public class GraphqlProjectApplication implements CommandLineRunner {
 
+	@Autowired
+	private BookService bookService;
 	public static void main(String[] args) {
 		System.out.println("Hi");
 		SpringApplication.run(GraphqlProjectApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Book b1 = new Book();
+		b1.setTitle("Complete Reference");
+		b1.setDesc("Learning Java");
+		b1.setPages(2000);
+		b1.setPrice(700);
+		b1.setAuthor("Narshima karumachi");
+
+		this.bookService.create(b1);
+	}
 }
